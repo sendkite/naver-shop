c863a6a6f1bb0cbb4d5cc168eb1cc5c8b532f5c6
sendkite <sendkite1@gmail.com>
2022-01-09 22:20:47 +0900

    Initial commit 환경설정

---
+++ src/main/java/com/sparta/navershop/NaverShopApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.navershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaverShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/navershop/NaverShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.navershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
