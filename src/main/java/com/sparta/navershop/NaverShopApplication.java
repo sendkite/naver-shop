b5fcda7bacbfc138893dbf85e0964ec29792c4f9
sendkite <sendkite1@gmail.com>
2022-01-09 22:34:45 +0900

    Feature 레거시 코드

--- src/main/java/com/sparta/navershop/NaverShopApplication.java
+++ src/main/java/com/sparta/navershop/NaverShopApplication.java
@@ -2,7 +2,9 @@ package com.sparta.navershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // @WebServlet 동작하게 함
 @SpringBootApplication
 public class NaverShopApplication {
 
