bcb8b784931b6c095277aa477894a14be7ad2b1f
sendkite <sendkite1@gmail.com>
2022-01-10 22:55:51 +0900

    Feature : AllInOneController 추가, UI 추가

---
+++ src/main/java/com/sparta/navershop/ProductMypriceRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.navershop;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProductMypriceRequestDto {
+    private int myprice;
+}
